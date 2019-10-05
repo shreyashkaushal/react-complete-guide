33d099575a6fed002690ca4c0bb79fc6aceb82a3
shreyashkaushal <shreyashkaushal10@gmail.com>
2019-10-05 14:04:40 +0530

    advance component tutorial

--- src/Persons/Person/Person.js
+++ src/Persons/Person/Person.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import './Person.css';
+import classes from  './Person.css';
 const person = (props) => {
    
     return (
-        <div className="Person" ><p onClick = {props.click}>hey i am {props.name} and i am {props.age} years old</p>
+        <div className={classes.Person} ><p onClick = {props.click}>hey i am {props.name} and i am {props.age} years old</p>
         <p>{props.children}</p>
         <input type ="text" onChange={props.changed} value={props.name} />
         </div>
